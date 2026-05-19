0c8269569d8d3f02a22585106b75043b8905a604
Esen Ergun <eergun@ucsd.edu>
2026-05-19 19:27:21 +0000

    log base e loss

--- fairseq/criterions/adaptive_loss.py
+++ fairseq/criterions/adaptive_loss.py
@@ -85,6 +85,7 @@ class AdaptiveLoss(FairseqCriterion):
         sample_size = utils.item(sum(log.get('sample_size', 0) for log in logging_outputs))
 
         metrics.log_scalar('loss', loss_sum / sample_size / math.log(2), sample_size, round=3)
+        metrics.log_scalar('loss_base_e', loss_sum / sample_size, sample_size, round=3)
         if sample_size != ntokens:
             metrics.log_scalar('nll_loss', loss_sum / ntokens / math.log(2), ntokens, round=3)
             metrics.log_derived('ppl', lambda meters: utils.get_perplexity(meters['nll_loss'].avg))
